e07647d810a51b1b6d991d427f6a66975747ce23
ramadhanyy <anggarahmatramadhani@gmail.com>
2022-11-28 01:25:06 +0700

    mini project SIGWEB M12

---
+++ data/FasilitasKesehatan_5.js
@@ -0,0 +1,22 @@
+var json_FasilitasKesehatan_5 = {
+"type": "FeatureCollection",
+"name": "FasilitasKesehatan_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nama": "Puskesmas Malata", "Kategori": "Puskesmas" }, "geometry": { "type": "Point", "coordinates": [ 119.38453486200001, -9.44792573299998 ] } },
+{ "type": "Feature", "properties": { "Nama": "Polindes Patola", "Kategori": "Polindes" }, "geometry": { "type": "Point", "coordinates": [ 119.444454936000042, -9.504475299999967 ] } },
+{ "type": "Feature", "properties": { "Nama": "Polindes Karaka", "Kategori": "Polindes" }, "geometry": { "type": "Point", "coordinates": [ 119.4176770790001, -9.520652974999956 ] } },
+{ "type": "Feature", "properties": { "Nama": "Poliklinik Mamboro", "Kategori": "Poliklinik" }, "geometry": { "type": "Point", "coordinates": [ 119.446830891000118, -9.573322956999965 ] } },
+{ "type": "Feature", "properties": { "Nama": "RSUD Waikabubak", "Kategori": "Rumah Sakit" }, "geometry": { "type": "Point", "coordinates": [ 119.37982693700009, -9.621853232999955 ] } },
+{ "type": "Feature", "properties": { "Nama": "RS Lende Moripa", "Kategori": "Rumah Sakit" }, "geometry": { "type": "Point", "coordinates": [ 119.414146106000089, -9.632313734999958 ] } },
+{ "type": "Feature", "properties": { "Nama": "Poliklinik Pertiwi", "Kategori": "Poliklinik" }, "geometry": { "type": "Point", "coordinates": [ 119.414032945000031, -9.631415512999975 ] } },
+{ "type": "Feature", "properties": { "Nama": "Puskesmas Puweri", "Kategori": "Puskesmas" }, "geometry": { "type": "Point", "coordinates": [ 119.453166478000071, -9.626406417999924 ] } },
+{ "type": "Feature", "properties": { "Nama": "Poliklinik Hupumada", "Kategori": "Poliklinik" }, "geometry": { "type": "Point", "coordinates": [ 119.471164641000087, -9.702708950999984 ] } },
+{ "type": "Feature", "properties": { "Nama": "Puskesmas Laihuruk", "Kategori": "Puskesmas" }, "geometry": { "type": "Point", "coordinates": [ 119.454620351, -9.701122832999941 ] } },
+{ "type": "Feature", "properties": { "Nama": "Puskesmas Padediwatu", "Kategori": "Puskesmas" }, "geometry": { "type": "Point", "coordinates": [ 119.397982607000017, -9.747864878999959 ] } },
+{ "type": "Feature", "properties": { "Nama": "Poliklinik Lamboya", "Kategori": "Poliklinik" }, "geometry": { "type": "Point", "coordinates": [ 119.35649053300007, -9.720925961999958 ] } },
+{ "type": "Feature", "properties": { "Nama": "Puskesmas Kabukarudi", "Kategori": "Puskesmas" }, "geometry": { "type": "Point", "coordinates": [ 119.353952866000085, -9.720757635999979 ] } },
+{ "type": "Feature", "properties": { "Nama": "Polindes Patiala", "Kategori": "Polindes" }, "geometry": { "type": "Point", "coordinates": [ 119.328302778000065, -9.746633129999957 ] } },
+{ "type": "Feature", "properties": { "Nama": "Puskesmas Gaura", "Kategori": "Puskesmas" }, "geometry": { "type": "Point", "coordinates": [ 119.257902956000066, -9.710858253999955 ] } }
+]
+}
